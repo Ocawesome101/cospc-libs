c4f78376be9f64901239ab79c09410d023e5ae86
Ocawesome101 <ocawesome101@gmail.com>
2021-11-11 18:38:46 -0500

    e

--- input.lua
+++ input.lua
@@ -13,16 +13,12 @@ function lib.poll(state)
   local sig = table.pack(os.pullEventRaw())
   if sig[1] == "timer" and id == state.lastTimerID then
     state.lastTimerID = nil
-    return false
   elseif sig[1] == "key" then
     state.pressed[sig[2]] = true
-    return true
   elseif sig[1] == "key_up" then
     state.pressed[sig[2]] = false
-    return true
-  else
-    return sig
   end
+  return sig
 end
 
 return lib
