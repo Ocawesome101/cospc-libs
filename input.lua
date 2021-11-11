73fe513989482a9e558f04e74a9b964aec36eaca
Ocawesome101 <ocawesome101@gmail.com>
2021-11-11 18:39:39 -0500

    lel bugs

--- input.lua
+++ input.lua
@@ -11,7 +11,7 @@ function lib.poll(state)
     state.lastTimerID = os.startTimer(0)
   end
   local sig = table.pack(os.pullEventRaw())
-  if sig[1] == "timer" and id == state.lastTimerID then
+  if sig[1] == "timer" and sig[2] == state.lastTimerID then
     state.lastTimerID = nil
   elseif sig[1] == "key" then
     state.pressed[sig[2]] = true
